23ae1a6c9741a2e8141818da867818f4fa4cfe2f
kristenvincent <kvincent2@wisc.edu>
2017-01-23 13:37:08 -0600

    fix legends

--- js/modules/m-section.js
+++ js/modules/m-section.js
@@ -518,7 +518,7 @@ var LegendLayerView = Backbone.View.extend({
 				view.model.attributes.maxRadius = parseFloat(range[range.length-1]);
 				//svg height should be whichever is larger, label heights or largest circle diameter
 				var heightArray = [
-					13 * range.length + 6, 
+					13 * range.length + 6,
 					parseFloat(range[range.length-1]) * 2 + 6
 				];
 				heightArray.sort(function(a,b){ return b-a });
@@ -646,7 +646,7 @@ var LegendLayerView = Backbone.View.extend({
 		this.css = css;
 		//set legend elements
 		var techniqueType = this.model.get('techniqueType');
-		this.techniques[techniqueType](this);		
+		this.techniques[techniqueType](this);
 		//style according to layer options
 		for (var style in css){
 			this.$el.children('rect').each(function(){
@@ -1346,7 +1346,7 @@ var ReclassifyView = ReexpressInputView.extend({
 			//set display and values based on number of classes
 			var display = i < scale.range().length-1 ? 'inline' : 'none';
 			this.$el.find('.class-break-inputs').append(cbTemplate({
-				index: i, 
+				index: i,
 				display: display
 			}));
 		};
@@ -1369,7 +1369,7 @@ var ReclassifyView = ReexpressInputView.extend({
 			setClassification = this.setClassification,
 			setNClasses = this.setNClasses,
 			setClassBreaks = this.setClassBreaks;
-		
+
 		//set reclassification listener on classification dropdown
 		this.$el.find('select[name=classification]').change(function(e, r){
 			//get classification parameters
@@ -1431,7 +1431,7 @@ var RecolorView = ReclassifyView.extend({
 			} else {
 				var hex = this.model.get('color');
 			};
-			//set default color of input 
+			//set default color of input
 			colorInput.attr('value', hex);
 			//hide color scale select
 			this.$el.find('.recolor select, .color-scale-palette, .color-scale-button').hide();
@@ -1565,7 +1565,7 @@ var RecolorView = ReclassifyView.extend({
 				});
 			};
 		}, this);
-		
+
 		//set listeners on each list element in color scale list
 		this.setColorSelectListeners(list);
 
@@ -1583,7 +1583,7 @@ var RecolorView = ReclassifyView.extend({
 			}, this);
 		}, this);
 		//go ahead with palette if first run
-		if (init){ 
+		if (init){
 			this.model.trigger('reclassified')
 		};
 	},
@@ -1794,7 +1794,7 @@ var LeafletMap = Backbone.View.extend({
 		leafletBaseLayer._leaflet_id = Math.round(Math.random()*10000);
 		var layerId = leafletBaseLayer._leaflet_id;
 		//only add first base layer to the map
-		if (i==0){ 
+		if (i==0){
 			leafletBaseLayer.addTo(this.map);
 			this.firstLayers[layerId] = leafletBaseLayer;
 		} else {
@@ -1860,7 +1860,7 @@ var LeafletMap = Backbone.View.extend({
 	},
 	setTechniques: function(dataLayerModel){
 		//variables needed by internal functions
-		var view = this, 
+		var view = this,
 			model = view.model,
 			map = view.map,
 			dataLayerOptions = dataLayerModel.get('layerOptions') || {};
@@ -2013,7 +2013,7 @@ var LeafletMap = Backbone.View.extend({
 						view.trigger('dataLayersDone');
 					};
 				};
-			};			
+			};
 		}, this);
 	},
 	setDataLayer: function(dataLayer, i){
@@ -2088,7 +2088,7 @@ var LeafletMap = Backbone.View.extend({
 			zoom = map.getZoom();
 		//remove out-of-bounds layers
 		map.eachLayer(function(layer){
-			if (!layer._url && 
+			if (!layer._url &&
 				(layer.options.minZoom > zoom || layer.options.maxZoom < zoom)
 			){
 				//remove but leave "shown" so it appears on zoom in
@@ -2161,7 +2161,7 @@ var LeafletMap = Backbone.View.extend({
 				}]
 			});
 		});
-		
+
 		//add close button
 		var closeButton = _.template( $('#close-button-template').html() );
 		$('.legend-control-container').prepend(closeButton());
@@ -2255,7 +2255,7 @@ var LeafletMap = Backbone.View.extend({
 					leafletView.trigger('pan');
 				};
 			};
-			//set timer to avoid autopan triggering	
+			//set timer to avoid autopan triggering
 			map.on('autopanstart', function(){
 				autopan = true;
 				setTimeout(function(){ autopan = false }, 500);
@@ -2732,7 +2732,7 @@ var LeafletMap = Backbone.View.extend({
 						$('#'+layerName+'-logic-div input').val('');
 						$('#'+layerName+'-logic-div input').prop('disabled', true);
 					}
-				};	
+				};
 			});
 
 			return controlView;
@@ -3219,7 +3219,7 @@ var LeafletMap = Backbone.View.extend({
 				};
 				if (interactions[interaction].toggle){
 					//render interaction toggle button
-					interactionToggleView.render();	
+					interactionToggleView.render();
 					//change the reset control title
 					$('.reset-control img').attr('title', 'reset map');
 				} else {
@@ -3296,7 +3296,7 @@ var LeafletMap = Backbone.View.extend({
 				go = false;
 				window.setTimeout(function(){go = true}, 100);
 			}
-		});	
+		});
 
 		//add initial tile layers
 		var baseLayers = this.model.get('baseLayers');
